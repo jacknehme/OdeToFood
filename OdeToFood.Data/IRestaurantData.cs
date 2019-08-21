00e3a0c67fd3034d77648c74ced39ecd8257c48f
jacknehme <jack.nehme@gmail.com>
2019-08-21 16:08:06 -0700

    Module 5 working on Saving and Committing Data

--- OdeToFood.Data/IRestaurantData.cs
+++ OdeToFood.Data/IRestaurantData.cs
@@ -1,8 +1,6 @@
 ﻿using OdeToFood.Core;
-using System;
 using System.Collections.Generic;
 using System.Text;
-using System.Linq;
 
 namespace OdeToFood.Data
 {
@@ -10,34 +8,9 @@ namespace OdeToFood.Data
     {
         IEnumerable<Restaurant> GetRestaurantsByName(string name);
         Restaurant GetById(int id);
-    }
-
-    public class InMemoryRestaurantData : IRestaurantData
-    {
-        readonly List<Restaurant> restaurants;
-        public InMemoryRestaurantData()
-        {
-            restaurants = new List<Restaurant>()
-            {
-                new Restaurant { Id = 1, Name = "Scott's Pizza", Location = "Maryland", Cuisine = CuisineType.Italian},
-                new Restaurant { Id = 2, Name = "Gina's Tacos", Location = "Califorina", Cuisine = CuisineType.Mexican},
-                new Restaurant { Id = 3, Name = "Akbar's Curry", Location = "Washington", Cuisine = CuisineType.Indian}
-
-            };
-        }
-
-        public Restaurant GetById(int id)
-        {
-            return restaurants.SingleOrDefault(r => r.Id == id);
-        }
-
-
-        public IEnumerable<Restaurant> GetRestaurantsByName(string name = null)
-        {
-            return from r in restaurants
-                   where string.IsNullOrEmpty(name) || r.Name.StartsWith(name)
-                   orderby r.Name
-                   select r;
-        }
+        Restaurant Update(Restaurant updatedRestaurant);
+        Restaurant Add(Restaurant newRestaurant);
+        Restaurant Delete(int id);
+        int Commit();
     }
 }
---
+++ OdeToFood.Data/SqlRestaurantData.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections.Generic;
+using OdeToFood.Core;
+
+namespace OdeToFood.Data
+{
+    public class SqlRestaurantData : IRestaurantData
+    {
+        private readonly OdeToFoodDbContext db;
+
+        public SqlRestaurantData(OdeToFoodDbContext db)
+        {
+            this.db = db;
+        }
+
+        public Restaurant Add(Restaurant newRestaurant)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public int Commit()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public Restaurant Delete(int id)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public Restaurant GetById(int id)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public IEnumerable<Restaurant> GetRestaurantsByName(string name)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public Restaurant Update(Restaurant updatedRestaurant)
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
